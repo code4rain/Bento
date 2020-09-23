585fa036cc95680623ca551b19a12c08922427df
Alex Kyoungdon Jang <code4rain@gmail.com>
2020-09-23 15:23:11 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '1302f89f692830543f3de1c601f87dd0';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 37.259731;
+    let longitude = 127.008949;
 
     getWeather(latitude, longitude);
 }
